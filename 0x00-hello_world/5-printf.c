8b530a72880894b0846907292dfd835c3731c2ba
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-08 19:04:52 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
   *main - Entry point
   *
-  *return : 0
+  *Return : Always 0
   */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
