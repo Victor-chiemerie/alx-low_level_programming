a1a2915f2789edd09480689a6d58ab073e73cd25
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-08 18:21:48 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+  \*
+    *first c program
+    *
+    */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
