aa8a5e5d0b2131a37f567cb146499e55e0323547
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-13 17:07:49 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,16 @@
  *
  *Return: always 0
  */
-int main (void)
+int main(void)
 {
       _putchar('_');
       _putchar('p');
-      _putchar('u'  );
-      _putchar('t'  );
-      _putchar('c'  );
-      _putchar('h'  );
-      _putchar('a'  );
-      _putchar('r'  );
-      putchar("\n");
+      _putchar('u');
+      _putchar('t');
+      _putchar('c');
+      _putchar('h');
+      _putchar('a');
+      _putchar('r');
+      _putchar('\n');
   return (0);
 }
