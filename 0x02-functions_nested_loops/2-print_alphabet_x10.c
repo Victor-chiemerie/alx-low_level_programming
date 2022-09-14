f7f0882aa505c0917732eac24de3fb831889e2df
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-13 17:57:45 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabets 10 times
+ *
+ *Return: void
+ */
+
+void print_alphabet_x10(void){
+int j;
+for (j=0; j<10; j++)
+  {
+    char ch = 'a';
+    for(int i = 0; i<26; i++){
+      _putchar(ch + i);
+    }    
+    _putchar('\n');
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 /* the main file contains the function prototype*/
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
