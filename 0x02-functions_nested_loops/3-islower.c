69c509be87b2ead45818568065e163017f8d5939
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-13 18:52:31 -0700

    33

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_islower - checks if a value is lower case or mot
+ *
+ *Return: 1 if true but returns 0 if false
+ */
+
+int _islower(int c){
+  if(c>=97 & c<=122)
+    return (1);
+  else 
+    return(0);
+}
