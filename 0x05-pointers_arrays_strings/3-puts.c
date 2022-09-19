fda1b95c85d6c41e55daa6b5d47c05c645610a3f
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-19 14:48:36 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <string.h>
+#include "main.h"
+#include <stdio.h>
 /**
  * _puts - prints the value of the string pointer parameter
  * @str: the String parameter
@@ -12,7 +13,7 @@ void _puts(char *str)
     int n = strlen(str);
     for(i = 0; i < n; i++)
     {
-        _putchar(str[i]);
+        putchar(str[i]);
     }
-    _putchar('\n');
+    putchar('\n');
 }
