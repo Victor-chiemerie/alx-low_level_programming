e2206c703eed32a96c07e576cd1602066a576a99
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-18 17:55:02 -0700

    j

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two values together
+ * @x: first parameter to be added
+ * @y: second parameter to be added
+ * 
+ * Return: the summation of the parameters
+ */
+int add(int x, int y)
+{
+    int res = x + y;
+     return (res);
+}
