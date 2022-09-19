fc0b4d0a8bf7fc91220310ac533aa5d99a251161
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-19 13:32:57 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - changes the value of the pointer parameter
- * @*n: the pointer address parameter
+ * @n: the pointer address parameter
  * 
  * Return: there is no return value
  */
