6dbbd5190c651b81c69983630046e5a75d472e01
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-19 15:12:24 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints the value of the string pointer parameter in reverse
+ * @s: the String parameter
+ * 
+ * Return: there is no return value
+ */
+void print_rev(char *s)
+{
+    int i;
+    int n = _strlen(s);
+    for(i = n; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+    _putchar('\n');
+} 
