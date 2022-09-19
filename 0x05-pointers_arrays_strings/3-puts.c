a2ba76df2b3610c8c14fdda5b5774f02f4c83a41
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-19 14:08:10 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - prints the value of the string pointer parameter
+ * @str: the String parameter
+ * 
+ * Return: there is no return value
+ */
+void _puts(char *str)
+{
+    puts(str);
+}
