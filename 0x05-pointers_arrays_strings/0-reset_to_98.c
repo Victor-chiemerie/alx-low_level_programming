ba038260122f30bc6749861db370b89503ab369b
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-19 13:27:52 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes the value of the pointer parameter
+ * @*n: the pointer address parameter
+ * 
+ * Return: there is no return value
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
