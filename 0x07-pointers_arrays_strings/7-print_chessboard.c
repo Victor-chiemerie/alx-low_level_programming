26b501eec1f0f3b6bf586643c413a96ae0f5354c
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-27 14:42:14 -0700

    6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard
+ * @a: the 2-dimensional array
+ *
+ * Return: there is no return value
+ */
+void print_chessboard(char (*a)[8])
+{
+  int i, j;
+  for ( i = 0; i < 8; i++)
+    {
+      for ( j = 0; j < 8; j++)
+	{
+	  _putchar(a[i][j]);
+	}
+      _putchar('\n');
+    }
+}
