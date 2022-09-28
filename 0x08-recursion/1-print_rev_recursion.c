89579ffde1125e2390fb382bfc63ff5cd9d0ba44
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-09-28 14:45:50 -0700

    1111

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+//#include "main.h"
 /**
  * _puts_recursion - prints a string in reverse
  * @s: the string to be printed
@@ -10,6 +10,6 @@ void _print_rev_recursion(char *s)
         if (*(s + 0))
         {
                 _print_rev_recursion(s + 1);
+                _putchar(*(s + 0));
         } 
-        _putchar(*(s + 0));
 }
