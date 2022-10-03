5ef2f89c8306c764dbc0aef80231b93897874b65
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-03 07:02:55 -0700

    a program that prints the arguments that are passed to it

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * 
+ * Return: always 0
+ */
+int main(int argc, char* argv[])
+{
+        int i;
+        for ( i = 0; i < argc; i++)
+        {
+            printf("%s\n", argv[i]);
+        }
+        return (0);
+}
