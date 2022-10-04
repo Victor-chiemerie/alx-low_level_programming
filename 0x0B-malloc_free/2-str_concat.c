67a5041cb4db8e98828932d456e613d201d56b5a
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-04 09:04:53 -0700

    22

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * str_concat - concancates two strings
+ * _strdup - concancates two strings
  * @s1: the first string to be concancated
  * @s2: the second string to be concancated
- *
+ * 
  * Return: a pointer to the string or NULL if str is NULL
  */
 char *str_concat(char *s1, char *s2)
@@ -18,18 +18,26 @@ int k;
 int sum;
 char *ch;
 
-if ((s1 == (NULL)) & (s2 == (NULL)))
+if (((s1 != NULL) | (s2 != NULL)))
 {
-return (NULL);
-}
+if (s1 != NULL)
+{
 do {
 n1++;
 } while (*(s1 + n1));
+}
+if (s2 != NULL)
+{
 do {
 n2++;
 } while (*(s2 + n2));
+}
 sum = n1 + n2;
 ch = malloc((sum + 1) * (sizeof(char)));
+if  (ch == NULL)
+{
+return (NULL);
+}
 for (j = 0; j < n1; j++)
 {
 *(ch + j) = *(s1 + j);
@@ -40,4 +48,7 @@ for (k = j, i = 0; k < sum; k++, i++)
 }
 *(ch + k) = '\0';
 return (ch);
+}
+else
+return (NULL);
 }
