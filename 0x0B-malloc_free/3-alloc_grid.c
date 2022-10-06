c1db535c0b451efd8c4aa18393192dcf9a891023
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-05 17:03:48 -0700

    33

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,38 +4,38 @@
  * alloc_grid - a pointer to a 2 dimensional array of integers
  * @width: the first string to be concancated
  * @height: the second string to be concancated
- * 
+ *
  * Return: a pointer to the array or NULL if any arg is NULL
  */
 int **alloc_grid(int width, int height)
 {
-  int i, j, k;
-  int **array;
+int i, j, k;
+int **array;
 
-  if  ((width < 1) | (height < 1))
-    {
-      return (NULL);
-    }
-  array = (int **)malloc(sizeof(int *) * height);
-  if  (array == 0)
-    {
-      free(array);
-      return (NULL);
-    }
-  for ( i = 0; i < height; i++)
-    {
-      *(array + i) = (int *)malloc(sizeof(int) * width);
-      if  (*(array + i) == 0)
-	{
-	  for(k = 0; k < i; k++)
-	  free(*(array + k));
-	  free(array);
-	  return (NULL);
-	}
-      for ( j = 0; j < width; j++)
-	{
-	  *(*(array + i) + j) = 0;
-	}
-    }
-  return (array);
+if  ((width < 1) | (height < 1))
+{
+return (NULL);
+}
+array = (int **)malloc(sizeof(int *) * height);
+if  (array == 0)
+{
+free(array);
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+*(array + i) = (int *)malloc(sizeof(int) * width);
+if  (*(array + i) == 0)
+{
+for (k = 0; k < i; i++)
+free(*(array + k));
+free(array);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+{
+*(*(array + i) + j) = 0;
+}
+}
+return (array);
 }
