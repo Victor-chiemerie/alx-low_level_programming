acae71204cdaa849a35ca851e10773e3f8723249
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-07 15:14:43 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 #include <string.h>
 /**
  * _atoi - converts strings consisting of numerical values to integer
@@ -9,35 +10,35 @@
  */
 int _atoi(char *s)
 {
-int n = strlen(s);
-int multiply = 1;
-int num;
-int i;
-int result = 0;
-int sign = 1;
-for (i = n; i >= 0; i--)
-{
-if ((s[i] > 47) & (s[i] < 58))
-{
-num = (char) s[i];
-num = num - 48;
-num *= multiply;
-result += num;
-multiply *= 10;
-}
-if (s[i] == 45)
-{
-sign *= -1;
-}
-if (s[i] == 66)
-{
-multiply = 1;
-result = 0;
-sign = 1;
-}
-}
-result *= sign;
-return (result);
+  int n = strlen(s);
+  int multiply = 1;
+  int num;
+  int i;
+  int result = 0;
+  int sign = 1;
+  for (i = n; i >= 0; i--)
+    {
+      if ((s[i] > 47) & (s[i] < 58))
+	{
+	  num = (char) s[i];
+	  num = num - 48;
+	  num *= multiply;
+	  result += num;
+	  multiply *= 10;
+	}
+      if (s[i] == 45)
+	{
+	  sign *= -1;
+	}
+      if (s[i] == 66)
+	{
+	  multiply = 1;
+	  result = 0;
+	  sign = 1;
+	}
+    }
+  result *= sign;
+  return (result);
 }
 
 /**
@@ -48,22 +49,22 @@ return (result);
  */
 void print_number(int val)
 {
-int rem;
-if (val < 0)
-{
-val = val * -1;
-_putchar('-');
-}
-if(val > 9)
-{
-print_number(val / 10);
-rem = val % 10;
-_putchar('0' + rem);
-}
-else
-{
-_putchar('0' + val);
-}
+  int rem;
+  if (val < 0)
+    {
+      val = val * -1;
+      _putchar('-');
+    }
+  if(val > 9)
+    {
+      print_number(val / 10);
+      rem = val % 10;
+      _putchar('0' + rem);
+    }
+  else
+    {
+      _putchar('0' + val);
+    }
 }
 
 /**
@@ -75,39 +76,39 @@ _putchar('0' + val);
  */
 int main(int argc, char **argv)
 {
-int result;
-int i, j;
+  int result;
+  int i, j;
 
-if (argc != 3)
-{
-_putchar('E');
-_putchar('r');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
-exit(98);
-}
-for (i = 1; i < argc; i++)
-{
-j = 0;
-while (*(*(argv + i) + j))
-{
-if ((*(*(argv + i) + j) < 48) | (*(*(argv + i) + j) > 57))
-{
-_putchar('E');
-_putchar('r');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
-exit(98);
-}
-j++;
-}
-}
-result = _atoi(*(argv + 1)) * _atoi(*(argv + 2));
-print_number(result);
-_putchar('\n');
-return (0);
+  if (argc != 3)
+    {
+      _putchar('E');
+      _putchar('r');
+      _putchar('r');
+      _putchar('o');
+      _putchar('r');
+      _putchar('\n');
+      exit(98);
+    }
+  for (i = 1; i < argc; i++)
+    {
+      j = 0;
+      while (*(*(argv + i) + j))
+	{
+	  if ((*(*(argv + i) + j) < 48) | (*(*(argv + i) + j) > 57))
+	    {
+	      _putchar('E');
+	      _putchar('r');
+	      _putchar('r');
+	      _putchar('o');
+	      _putchar('r');
+	      _putchar('\n');
+	      exit(98);
+	    }
+	  j++;
+	}
+    }
+  result = _atoi(*(argv + 1)) * _atoi(*(argv + 2));
+  print_number(result);
+  _putchar('\n');
+  return (0);
 }
