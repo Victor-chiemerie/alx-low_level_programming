8631b175f7d243f7db129a2ee6d9ff17908afffd
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-07 09:41:55 -0700

    weldone

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * _calloc - allocates memory for an array, using malloc
  * @nmemb: the number of the array elements
