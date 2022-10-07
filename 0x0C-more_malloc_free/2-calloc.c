1c9622dbd06d42b01cd46998090280a295e175b7
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-07 09:32:31 -0700

    well

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,22 +12,22 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *array;
-int i = 0;
+unsigned int i = 0;
 
-if ((nmemb == 0) | (size == 0))
+if ((nmemb == 0) || (size == 0))
 {
 return (NULL);
 }
- array = (char *)malloc((nmemb * size) + 1);
+array = malloc((nmemb * size) + 1);
 if (array == 0)
 {
 return (NULL);
 }
-while (*(array + i))
+while (i < nmemb)
 {
 *(array + i) = 0;
 i++;
 }
- *(array + i) = '\0';
+*(array + i) = '\0';
 return (array);
 }
