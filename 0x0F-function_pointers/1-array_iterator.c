16a0cf0cf8a361e443ff90cad1de713c2af1daf7
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-13 06:11:03 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through an array
+ * @array: the array to be iterated
+ * @size: the number of elements in the array
+ * @action: a pointer to a function to be executed
+ *
+ * Return: nothing
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(*(array + i));
+		}
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef MY_FILE
 #define MY_FILE
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
