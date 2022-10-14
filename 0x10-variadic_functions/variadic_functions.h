2bd835f5eb1bcc8d43141fbb3edaf2cd28d59a34
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-13 20:08:50 -0700

    good

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
 #ifndef CALC_H
-#define CACL_H
+#define CALC_H
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
