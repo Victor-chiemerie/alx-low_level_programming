925fd954ee616020d95216076345ecf8022a216a
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-22 07:10:43 -0700

    did task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_list - wipes all the elements of a list from memory
+ * @head: a pointer to the first element of the list
+ *
+ * Return: Nothing
+*/
+void free_list(list_t *head)
+{
+    if (head->next != NULL)
+    {
+        free_list((head->next));
+    }
+    free(head);
+}
