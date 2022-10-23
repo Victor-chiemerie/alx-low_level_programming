1d2d379979ebe748b6c9260a08a9b71c2debe7e6
Victor-chiemerie <victorchiemerie02@gmail.com>
2022-10-22 17:19:23 -0700

    finished project

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void startup(void) __attribute__ ((constructor));
+
+/**
+ * startup - the function to be executed before main
+ *
+ * Return: Nothing
+*/
+void startup(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
+
